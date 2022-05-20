4ce13b754c089c13183f4a071edcdf399cd32125
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-20 00:59:51 -0700

    Create the Items homepage viewsfile

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList
 {
@@ -10,18 +12,24 @@ namespace ToDoList
   {
     public Startup(IWebHostEnvironment env)
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json"); //this line replaces .AddEnvironmentVariables();
+        Configuration = builder.Build();
     }
 
     public IConfigurationRoot Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+        services.AddMvc();
+
+        //New code
+        services.AddEntityFrameworkMySql()
+            .AddDbContext<ToDoListContext>(options => options
+            .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
+
 
     public void Configure(IApplicationBuilder app)
     {
